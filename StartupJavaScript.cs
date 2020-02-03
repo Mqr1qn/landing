0469a1ece6cc7bc2bffc364c9b2244a61e87793f
Mqr1qn <49136196+Mqr1qn@users.noreply.github.com>
2020-02-03 16:19:25 -0300

    Add files via upload

---
+++ StartupJavaScript.cs
@@ -0,0 +1,54 @@
+﻿// Unused usings removed
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Landing.DataAccesLayer.Models;
+using Landing.DataAccesLayer.Data;
+
+namespace LandingApi
+{
+    public class StartupJavaScript
+    {
+        public StartupJavaScript(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<LandingContext>(opt =>
+                opt.UseLazyLoadingProxies()
+               .UseSqlServer(Configuration.GetConnectionString("LandingContext")));
+            services.AddControllers();
+        }
+
+        #region snippet_configure
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+        #endregion
+    }
+}
